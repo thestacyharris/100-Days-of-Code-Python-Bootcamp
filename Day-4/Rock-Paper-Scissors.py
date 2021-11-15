91b85590df7afea114e7b8155f57900d70eba48a
thestacyharris <40574323+thestacyharris@users.noreply.github.com>
2021-11-15 13:07:49 -0500

    Create Rock-Paper-Scissors.py

---
+++ Day-4/Rock-Paper-Scissors.py
@@ -0,0 +1,50 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+
+import random
+
+game_images = [rock, paper, scissors]
+
+user_choice = int(input("What do you chose? Type 1 for Rock, 2, for Paper, or 3 for Scissors.\n"))
+
+computer_choice = random.randint(1,3)
+
+print("You chose: \n" + game_images[user_choice -1] + "\n")
+print("The computer chose: \n" + game_images[computer_choice -1] + "\n")
+
+if user_choice == 1 and computer_choice == 3:
+  print("You win.")
+elif user_choice == 3 and computer_choice == 1:
+  print("You lose.")
+elif int(user_choice) == int(computer_choice):
+  print("It is a draw.")
+elif int(user_choice) > int(computer_choice):
+  print("You win.")  
+else:
+  print("You lose.")  
